9eb73a8dfbfafa2863ad22785411a7bd42a66589
gabriel <gabriel@staketogether.org>
2024-08-15 22:42:47 -0300

    feat: start the integration with the pagbank

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -5,8 +5,7 @@ import * as crypto from 'crypto';
 import { InjectModel } from '@nestjs/sequelize';
 import { EmailModel } from './email.model';
 import { AccountAuthModel } from '../account-auth/account-auth.model';
-import * as fs from 'fs';
-import * as path from 'path';
+import { isMD5 } from 'validator';
 
 @Injectable()
 export class EmailService {
@@ -24,12 +23,6 @@ export class EmailService {
 
   logger = new Logger(EmailService.name);
 
-  private getMailHtml(recoveryKey: number): string {
-    const templatePath = path.join(__dirname, '.', 'template', 'email-template.html');    let html = fs.readFileSync(templatePath, 'utf8');
-    html = html.replace('{{recoveryKey}}', recoveryKey.toString());
-    return html;
-  }
-
   async sendRefactorCodeMail(to: string, accountAuth: AccountAuthModel): Promise<void> {
     const recoveryKey = this.generateVerificationCode()
 
@@ -37,9 +30,66 @@ export class EmailService {
       from: this.configService.get<string>('EMAIL_USER'),
       to: to,
       subject: "Código para trocar sua senha",
-      html: this.getMailHtml(recoveryKey),
+      html: `
+      <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+</head>
+<body style="margin: 0; padding: 0;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin: 0; padding: 0;">
+  <tr>
+    <td align="center">
+      <table width="600" border="0" cellspacing="0" cellpadding="0" style="width: 600px;">
+        <tr>
+          <td style="background-color: #e88b00; border-radius: 20px; padding: 20px;">
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="center" style="background-color: #ffffff; border-radius: 20px; padding: 20px; width: 100%; height: 200px;">
+                  <span style="font-family: 'Comic Sans MS', 'Arial', sans-serif; color: #E88B00FF; font-size: 60px; position: relative; top: -30px;">
+                    Delicias Caseiras Tia nana
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="font-family: Arial, sans-serif; font-size: 50px; color: #ffffff; padding: 10px; position: relative; top: -30px; left: -120px;">
+                  Olá, ${accountAuth.accountUser.name}
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="font-family: Arial, sans-serif; font-size: 18px; color: #ffffff; padding: 10px;">
+                  Use o código abaixo para trocar a senha da sua conta
+                </td>
+              </tr>
+              <tr>
+              <td align="center" style="padding: 20px;">
+                <div style="background-color: #ffffff; border-radius: 20px; width: 300px; height: 100px; text-align: center; font-family: 'Comic Sans MS', 'Comic Sans', cursive; font-size: 30px;">
+                  <table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td align="center" valign="middle">
+                        ${recoveryKey}
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </td>
+              </tr>
+              <tr>
+                <td align="center" style="font-family: Arial, sans-serif; font-size: 18px; color: #ffffff; padding: 10px;">
+                  Se você não solicitou esta troca, por favor ignore esta mensagem.
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
+    `,
     };
-
 
 
     try {
